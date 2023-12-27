9a1cf96b73f1764a1874d985519904a4811a6e14
Agnichaturvedi <chaturvedi.ag@northeastern.edu>
2023-12-27 02:53:44 -0500

    updated backend end point

--- event-share-app/src/services/base-service.ts
+++ event-share-app/src/services/base-service.ts
@@ -1,5 +1,5 @@
 
-const baseURL = 'https://eventshare-nc92.onrender.com';
+const baseURL = 'https://eventshare.onrender.com/';
 export const search = async<T>(path: string, params: any): Promise<T[]> => {
     const query: URLSearchParams = new URLSearchParams(params);
     const response = await fetch(baseURL+path+query,{
